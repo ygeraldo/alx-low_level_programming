50e45098526ace1bbb732fe0e926e10220e5798a
root <you@example.com>
2022-06-30 15:26:48 -0700

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.>
+int main()
+{
+	//string initialisation
+    char Mystr[] = "Programming is like building a multilingual puzzle\n"
+
+    puts(Mystr);  //writting the string to sdtout
+
+    return 0;
+}    

c5eb05670f93bcf4cc6aed6a0f73ae61c4579d83
root <you@example.com>
2022-07-01 18:00:11 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.>
+/**
+ * main - Task 3 prints the alphabet in lowercase, and the in uppercase
+ * followed by a new line
+ *
+ * return: 0
+ **/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

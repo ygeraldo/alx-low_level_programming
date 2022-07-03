9b2ce9e1139c0183828769746b5d8074d18c81ac
root <you@example.com>
2022-07-03 07:00:06 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h> 
+#include <stdio.h>
+/*
+ *main - assign a random number to the variable num each time it is executed
+ *only using putchar and without char variables.
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	
+	putchar ("\n");  
+
+	return (0);
+}

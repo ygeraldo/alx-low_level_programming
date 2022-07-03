b7f2188ef7ca8a0260b1bc36a6ec92f06e0a4e60
root <you@example.com>
2022-07-03 05:34:40 -0700

    all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a == 0 || a <= 10)
+		print("%d", a--)
+	return 0;
+}

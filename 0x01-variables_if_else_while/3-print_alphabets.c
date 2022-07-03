98b3fcfe73aa9876447583c90bcbf63b8b72d85b
root <you@example.com>
2022-07-03 07:32:51 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar (letter);
 
@@ -18,5 +18,6 @@ int main(void)
 		putchar (letter);
 	
 	putchar ('\n');
+
 	return (0);
 }

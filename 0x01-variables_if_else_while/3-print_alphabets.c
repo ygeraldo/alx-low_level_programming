78e60155854947e917f8f665bc9d4e38cf252126
root <you@example.com>
2022-07-03 02:25:47 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ *main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ *Return: Always 0.
  */
 
 int main(void)

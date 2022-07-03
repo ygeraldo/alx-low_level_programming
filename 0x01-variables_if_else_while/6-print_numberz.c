72eb463bf3dc48093b1351f247c3bc4687ef22fa
root <you@example.com>
2022-07-03 11:58:13 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,7 +5,7 @@
  *main - Prints all single digit numbers of base 10 starting from 0,
  *only using putchar and without char variables.
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 
 int main(void)
@@ -14,8 +14,6 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	
 	putchar('\n');  
-
 	return (0);
 }

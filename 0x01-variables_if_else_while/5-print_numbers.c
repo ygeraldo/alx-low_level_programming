4ec5f5a0d3ad7660cae004b10e52edea6f722714
root <you@example.com>
2022-07-03 06:29:00 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
+ *main - assign a random number to the variable num each time it is executed
  *prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int num;

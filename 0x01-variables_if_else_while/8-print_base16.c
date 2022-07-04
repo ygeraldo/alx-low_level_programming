485c1dec51ff4858d55813391022995129faa0df
root <you@example.com>
2022-07-04 12:54:35 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,7 +14,8 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	for (letter = 'a'; letter <= 'f'; letter++);
+
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);

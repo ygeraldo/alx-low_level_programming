726a85966bd0d091478066f02a792a7a563065d7
root <you@example.com>
2022-07-06 18:57:42 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,18 @@
 #include "main.h"
+
+
 /**
- * main - calls other functions
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+
 int main(void)
 {
-	positive_or_negative()	
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN.H
 
-int main(void)
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */

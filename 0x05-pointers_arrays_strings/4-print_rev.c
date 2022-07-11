3d47558c2cef3455250c06ac3f9dd393b60c2dc6
root <you@example.com>
2022-07-11 14:15:41 -0700

    Prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  *print_rev - Prints a string in reverse.
  *
  *@s: The string to be printed.
  */
-void print_rev(char *s)
-{
+void print_rev(char *s
+}
 	int i;
 
 	for (i = _strlen(s) - 1; i >= 0; i--)

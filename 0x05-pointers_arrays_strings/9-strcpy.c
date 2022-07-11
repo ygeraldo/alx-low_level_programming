d30bc039f563c28a023910c6709ab0e03896e967
root <you@example.com>
2022-07-11 16:55:06 -0700

    function that prints n elements of an array of integers, followed by a new line

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,13 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-	int index = 0;
+	int i = 0;
 
-	while (src[index])
+	while (*(src + i))
 	{
-		dest[index] = src[index];
-		index++;
+		*(dest + i) = *(src + i);
+		i++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }

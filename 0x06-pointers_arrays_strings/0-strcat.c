c5cc28bcc8b15a07803dd485404424cfdfa3a421
root <you@example.com>
2022-07-14 08:50:57 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strcat - concatenates two strings.
  *
@@ -8,5 +9,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char dest[] = "Hello ";
+	char src[] = "World!\n";
 
+	strcat(destination,source);
+	_putchar("Concatenated String: %s\n", destination);
+
+	return (dest);
 }

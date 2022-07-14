2c038d9ef27fc5e951528883544d54e84aa94ab7
root <you@example.com>
2022-07-14 06:43:06 -0700

    More pointers, Arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ *
+ *@ds: the two string
+ *
+ *Return: Pointer 
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+More pointers, arrays, string.
+In this new project I will do some tasks that cover pointers, arrays, string.
+Just follow me and build Togother this project.

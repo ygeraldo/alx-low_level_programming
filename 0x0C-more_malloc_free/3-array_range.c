45e103b1b39d9f7c8db73874f24c97ea0cd99358
ygeraldo <seveyawa@gmail.com>
2022-07-29 05:32:00 +0000

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,24 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *array_range - creates an array of integers.
+ *array_range - range of arrays
  *@min: min of arrange
  *@max: max of arrays
- *
  *Return: int value
  */
 int *array_range(int min, int max)
 {
-	int *tab = 0;
+	int *table = 0;
 	int i = 0;
 
 	if (min > max)
-		return (0);
-	tab = (int *)malloc(sizeof(int) * (max - min + 1));
-	if (!tab)
-		return (0);
-
+		return (NULL);
+	table = (int *)malloc(sizeof(int) * (max - min + 1));
+	if (!table)
+		return (NULL);
 	for (i = 0; i < (max - min + 1); i++)
-			tab[i] = min + i;
-	return (tab);
+		table[i] = min + i;
+	return (table);
 }

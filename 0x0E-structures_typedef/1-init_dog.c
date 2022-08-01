fa62bcb98156b4db52e8b4154b4a021c5a73a882
ygeraldo <seveyawa@gmail.com>
2022-08-01 18:49:55 +0000

    How to access elements of structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,4 +16,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->owner = owner;
 	}
 }
-		
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ struct dog
  *dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
